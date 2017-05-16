b4d0a925a0cafa6ed2f1c995c23aa131f2987ded
joshua1988 <jangkeehyo@gmail.com>
2017-05-16 23:34:55 +0900

    Increased the index of for iteration and added style effect at the end to make it visible

--- timeline/crp-optimize-loop.js
+++ timeline/crp-optimize-loop.js
@@ -1,4 +1,5 @@
-for (var i = 0; i < 10000; i++) {
+for (var i = 0; i < 50000; i++) {
   console.log(i + " 번 반복");
 }
 document.write(" private academy - ");
+document.querySelector('span').style.color = "blue";
